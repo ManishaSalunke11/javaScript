99ee0e107a0063cf889d4322d1da273ece5d5684
ManishaSalunke <msalunke928@gmail.com>
2023-05-06 12:43:43 +0530

    assignments

---
+++ 03_typeOf.js
@@ -0,0 +1,21 @@
+var bankName;
+console.log("Value and Type before initialization");
+console.log(bankName, typeof bankName);
+
+bankName = "SBI Bank";
+console.log("\n Value and Type after initialization");
+console.log(bankName, typeof bankName);
+
+var marks = '90%';
+console.log("\n datatype and value is:")
+console.log(typeof marks, marks);
+
+var isWorking;
+console.log("\nvalue of isWorking before initialization-->", isWorking);
+isWorking = true;
+console.log("value of isWorking after initialization-->", isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("\n value and datatype of totalCount is-->", totalCount, typeof totalCount);
+totalCount = 107;
+console.log("\n value and datatype of totalCount is-->", totalCount, typeof totalCount);
