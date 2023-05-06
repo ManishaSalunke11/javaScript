ad3cf00705899d43a9243d8025f6cf0d31fc7005
ManishaSalunke11 <msalunke928@gmail.com>
2023-05-06 08:36:28 +0530

    data types

--- premitive.js
+++ premitive.js
@@ -1,3 +1,5 @@
+// Primitive Type
+// 1. Number
 var age = 31;
 var weight = 78;
 var height = 6.1;
@@ -9,8 +11,8 @@ console.log(typeOfAge);
 console.log(typeof speckNumber);
 
 
-console.log(age);
-console.log(weight);
+console.log("Age:", age);
+console.log("Weight:", weight);
 console.log(height);
 console.log(speckNumber);
 
@@ -20,3 +22,25 @@ var knowAngular = true;
 var isSoftwareEngineer = true;
 var isBachelor = false; 
 console.log(isBachelor);
+var typeOfIsMarried = typeof isMarried;
+console.log("Type of isMarried:", typeOfIsMarried);
+
+// 3. String --> Sequence of character enclosed in '' or ""
+var firstName = "Bill";
+var position = 'Microsoft Co-founder'; 
+var typeOfFirstName = typeof firstName;
+var city = '';
+console.log( "Type of city ->", typeof city);
+console.log( "Type of variable -> firstName: ", typeOfFirstName );
+console.log("Type of variable --> position: " , typeof position);
+
+//2. Special Data type
+// 1. Undefined
+var address;
+console.log("Address: ", address);
+console.log("type of variable address is", typeof address);
+
+// 2. null
+var pin = null;
+console.log("Value of variable -> pin: ", pin);
+console.log('Type of variable pin is:', typeof pin);
