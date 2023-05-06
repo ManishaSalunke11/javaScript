2293b97d63d0def6315543048b5c5f216813e00e
ManishaSalunke11 <msalunke928@gmail.com>
2023-05-06 08:01:43 +0530

    Create premitive.js

---
+++ premitive.js
@@ -0,0 +1,22 @@
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+console.log(age, weight, height, speckNumber);
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+console.log(typeof speckNumber);
+
+
+console.log(age);
+console.log(weight);
+console.log(height);
+console.log(speckNumber);
+
+// 2. Boolean --> true or false
+var isMarried = true;
+var knowAngular = true; 
+var isSoftwareEngineer = true;
+var isBachelor = false; 
+console.log(isBachelor);
