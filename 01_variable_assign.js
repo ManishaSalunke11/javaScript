911bd51bd78a15c60caa5da516e470b4987332f0
ManishaSalunke <msalunke928@gmail.com>
2023-05-06 11:05:43 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,22 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Manisha Salunke";
+console.log(myFullName);
+var myFullName = "Manisha Dilip Salunke";
+console.log(myFullName);
+var myLove = '"I love only JS"';
+console.log(myLove);
+var firstName = "Manisha";
+console.log(firstName);
+var lastName = "Salunke";
+console.log(lastName);
+var age = "33";
+console.log(age);
+var address = "Pirangut,Pune."
+console.log(address);
+var pincode = "412115";
+console.log(pincode);
+var gender = "Female";
+console.log(gender);
+var isMarried = "yes";
+console.log(isMarried);
