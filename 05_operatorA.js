082a88f82240acba2d5ed4060bfe136d4dd14b5b
ManishaSalunke <msalunke928@gmail.com>
2023-05-16 06:50:51 +0530

    Update 05_operatorA.js

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -1,7 +1,6 @@
 function squareOfWordLength(string) {
     console.log("Given string:", string);
-    var split = string.split(" ");
-    square = split.length * split.length;
+    square = string.length * string.length;
     console.log("Square of words in this string is:", square);
     return (square);
 
@@ -13,7 +12,9 @@ squareOfWordLength("Developer Smart");
 function useOfOperatorsOnString() {
     var givenString = "I am Angular Developer";
     console.log("Given string is:", givenString);
+    console.log("Length of string is:", givenString.length);
     var split = givenString.split(" ");
+    console.log("words are:", split);
     console.log("String length divided by total no. of words is:", givenString.length / split.length);
     console.log("String length multiplied by total no. of words is:", givenString.length * split.length);
 }
