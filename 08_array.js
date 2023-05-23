83f2119460bc6a1996ceb99a2b17988ae1ba1e79
ManishaSalunke11 <msalunke928@gmail.com>
2023-05-23 08:18:26 +0530

    Update 08_array.js

--- 08_array.js
+++ 08_array.js
@@ -16,3 +16,25 @@ console.log(`Third index value: ${thirdIndexValue}`);
 let arrayLength = arrayOfRollNumbers.length;
 const lastElement = arrayOfRollNumbers[arrayLength-1];
 console.log(`Last element: ${lastElement}`);
+
+
+
+
+let arrayOfRollNumbers = [67, 56, 78, 90, 60, 90, 40];
+console.log(`Type of array: ${typeof arrayOfRollNumbers}`);
+
+let lengthOfArray = arrayOfRollNumbers.length;
+console.log(`Length of array is : ${lengthOfArray}`);
+
+// Accessing array element
+const zerothIndexValue = arrayOfRollNumbers[0];
+console.log(`Zeroth index value: ${zerothIndexValue}`);
+
+// Access the array element 90
+const thirdIndexValue = arrayOfRollNumbers[3];
+console.log(`Third index value: ${thirdIndexValue}`);
+
+// Accessing last element
+let arrayLength = arrayOfRollNumbers.length;
+const lastElement = arrayOfRollNumbers[arrayLength-1];
+console.log(`Last element: ${lastElement}`);
