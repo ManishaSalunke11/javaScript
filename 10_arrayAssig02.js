bd490edaedf822ece5e3cee9c09da863d3407849
ManishaSalunke <msalunke928@gmail.com>
2023-05-24 16:20:10 +0530

    Create 10_arrayAssig02.js

---
+++ 10_arrayAssig02.js
@@ -0,0 +1,12 @@
+const arrayNumbers = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+console.log(`Length of array is:${arrayNumbers.length}`);
+console.log(`First Element is:${arrayNumbers[0]}`);
+console.log(`Last Element is:${arrayNumbers[10]}`);
+var thirdLastElement = arrayNumbers[arrayNumbers.length - 3];
+console.log(`Third Last Element is:${thirdLastElement}`);
+for (let index = 0; index < arrayNumbers.length; index++) {
+    if (arrayNumbers[index] % 2 == 0) {
+        console.log(arrayNumbers);
+    }
+
+}
