b3ff1ef97f5c0194e6e6ff62363b8ea1a7315bfb
ManishaSalunke <msalunke928@gmail.com>
2023-05-25 11:33:56 +0530

    Update 10_arrayAssig02.js

--- 10_arrayAssig02.js
+++ 10_arrayAssig02.js
@@ -4,9 +4,10 @@ console.log(`First Element is:${arrayNumbers[0]}`);
 console.log(`Last Element is:${arrayNumbers[10]}`);
 var thirdLastElement = arrayNumbers[arrayNumbers.length - 3];
 console.log(`Third Last Element is:${thirdLastElement}`);
+console.log(`Even numbers in array are:`);
 for (let index = 0; index < arrayNumbers.length; index++) {
     if (arrayNumbers[index] % 2 == 0) {
-        console.log(arrayNumbers);
+        console.log(arrayNumbers[index]);
     }
 
 }
