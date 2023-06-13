5f19020e00a355467f840937c7bc68fcaccc5d62
ManishaSalunke <msalunke928@gmail.com>
2023-06-13 11:48:45 +0530

    Update palindromeAssig.js

--- palindromeAssig.js
+++ palindromeAssig.js
@@ -8,5 +8,21 @@ function isPalindrome(str) {
     }
     return 'It is a palindrome';
 }
-
-isPalindrome('madam');
+console.log("Given string is:madam");
+const result1 = isPalindrome("madam");
+console.log(result1);
+console.log("Given string is:141");
+const result2 = isPalindrome(141);
+console.log(result2);
+console.log("Given string is:Sunday");
+const result3 = isPalindrome("Sunday");
+console.log(result3);
+console.log("Given string is:mom");
+const result4 = isPalindrome("mom");
+console.log(result4);
+console.log("Given string is:listen");
+const result5 = isPalindrome("listen");
+console.log(result5);
+console.log("Given string is:dad");
+const result6 = isPalindrome("dad");
+console.log(result6);
